14d470a5be0096c7754c7936ba0dac9c744bbbdf
heshaoshuai <hss1741309417@163.com>
2018-03-20 17:47:50 +0800

    :sparkles: feat(1.4): 好好
    好好演示
    
    refs
    
    Signed-off-by: heshaoshuai <hss1741309417@163.com>

--- src/main/java/ShaoShuaiTest.java
+++ src/main/java/ShaoShuaiTest.java
@@ -3,5 +3,6 @@ public class ShaoShuaiTest {
         System.out.println("今天是周二！！！");
         System.out.println("你好啊！！！");
         System.out.println("演示");
+        System.out.println("好好的演示下git");
     }
 }
