b99a505a762b3bde5b58e9f14b8f7ef1f8f57d6d
heshaoshuai <hss1741309417@163.com>
2018-03-20 17:41:34 +0800

    :sparkles: feat(1.3): 演示
    演示git
    
    refs
    
    Signed-off-by: heshaoshuai <hss1741309417@163.com>

--- src/main/java/ShaoShuaiTest.java
+++ src/main/java/ShaoShuaiTest.java
@@ -2,5 +2,6 @@ public class ShaoShuaiTest {
     public static void main(String[] orgs) {
         System.out.println("今天是周二！！！");
         System.out.println("你好啊！！！");
+        System.out.println("演示");
     }
 }
